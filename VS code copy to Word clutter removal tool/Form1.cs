607ac7babda50321f73e97426de4c74dd88287c4
vsboy <41285126+vsboy@users.noreply.github.com>
2019-01-06 22:40:05 +0800

    first commitit

---
+++ VS code copy to Word clutter removal tool/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace VS_code_copy_to_Word_clutter_removal_tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //获取剪切板内容
+            IDataObject dataObject = Clipboard.GetDataObject();
+            if (dataObject.GetDataPresent(DataFormats.Rtf))
+            {
+                //去除RTF格式
+                string rtf = dataObject.GetData(DataFormats.Rtf) as string;
+                //以Regex.Replace去除多余字符(注: 不管是否有问题，一律强制处理)
+                string fixedRtf =Regex.Replace(rtf, @"\\uinput2(?<uc>\\u-?\d*)\s..",(m) =>
+                {
+                    return m.Groups["uc"].Value + "?";
+                });
+                //另建新DataObject物件
+                DataObject newDataObject = new DataObject();
+                //RTF格式用修正后的字符串，其余依原值
+                foreach (String t in dataObject.GetFormats())
+                    newDataObject.SetData(t,
+                    t == "Rich Text Format" ? fixedRtf :
+                    dataObject.GetData(t));
+                //将修正后内容写入剪贴簿
+                Clipboard.SetDataObject(newDataObject, true);
+                MessageBox.Show("Take Out Pessy Code successful!");
+            }
+        }
+    }
+}
---
+++ VS code copy to Word clutter removal tool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VS_code_copy_to_Word_clutter_removal_tool
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
